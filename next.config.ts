6eaf1a75bc721c29ddc3156f739094a4b79952c0
SierraMa1 <mariasierrasanchez1990@gmail.com>
2024-11-18 20:49:23 +0100

    first commit

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  transpileDependencies: true, 
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/SierraMa1/' //nombre de tu proyecto GitHub
+    : '/'
 };
 
 export default nextConfig;
