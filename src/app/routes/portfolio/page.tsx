511c3911f3f9c8676ed85c01535dd078ec3cc21d
SierraMa1 <mariasierrasanchez1990@gmail.com>
2024-11-25 20:03:11 +0100

    new changes

--- src/app/routes/portfolio/page.tsx
+++ src/app/routes/portfolio/page.tsx
@@ -1,9 +1,9 @@
-/* import Image from "next/image";
+import Image from "next/image";
 import Link from "next/link";
 
-import WaveImage from "@/components/ave";
+// import WaveImage from "@/components/ave";
 
-import PortfolioBox from "@/components/portfolio-box";
+// import PortfolioBox from "@/components/portfolio-box";
 
 
 const PortfolioPage = () => {
@@ -13,15 +13,15 @@ const PortfolioPage = () => {
             <div className="flex flex-col justify-center h-full">
                 <h1 className="text-2xl leading-tight text-center md:text-4xl md:mb-5">Mis últimos <span className="font-bold text-secondary">trabajos realizados</span></h1>
 
-                <div className="relative z-10 grid max-w-5xl gap-6 mx-auto mt-4 md:grid-cols-4">
+                {/* <div className="relative z-10 grid max-w-5xl gap-6 mx-auto mt-4 md:grid-cols-4">
                     {dataPortfolio.map((data) => (
                         <PortfolioBox key={data.id} data={data} />
                     ))}
-                </div>
+                </div> */}
             </div>
-            <WaveImage />
+            {/* <WaveImage /> */}
             </>
     );
 }
 
-export default PortfolioPage; */
+export default PortfolioPage; 
